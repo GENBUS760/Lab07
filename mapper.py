d9121dd3523c7dd36e0198e36501e521f7939187
CorneliaChu <zchu26@wisc.edu>
2021-12-14 19:53:46 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -5,6 +5,13 @@ import sys
 def mapper():
     for line in sys.stdin:
         data = line.strip().split(",")
+        count = 0
+        for i in data:
+            count += 1
+        if count < 4:
+            continue
+        if count > 4:
+            data = data[0:4]
         timestamp, city, item, cost = data
         print(city + "," + cost)
 
